9e68fc949794f4e647937398058f08c29777f9d7
caio.viga <caio.viga@dmz.org.br>
2022-06-21 16:54:04 -0400

    Heroku 4

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,6 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.launch()
 
-new CronJob(
-	'0 14 * * * *',
-	function() {
-		bot.telegram.sendMessage(process.env.CHAT_ID, "Então... Algum progresso? No que vocês andam trabalhando?")
-	},
-	null,
-	true,
-	'America/Los_Angeles'
-);
+//bot.telegram.sendMessage(process.env.CHAT_ID, "Então... Algum progresso? No que vocês andam trabalhando?");
+
+console.log("teste!");
